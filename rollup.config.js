a88acce7f53696aa4034c58507bfb96ad3399bf2
candicecz <czech.candice@gmail.com>
2023-06-16 12:09:11 -0300

    chore: update packages

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
 import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
-import {terser} from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 export default [
   {
